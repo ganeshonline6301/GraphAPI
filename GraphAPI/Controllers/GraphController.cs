d3aeaf804d70e4c9ddaecb9731526db404b20492
Ganesh Sri Surya <robinganesh007@gmail.com>
2025-01-19 00:32:24 +0530

    new graphcontroller created

---
+++ GraphAPI/Controllers/GraphController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraphAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GraphController : ControllerBase
+    {
+    }
+}
